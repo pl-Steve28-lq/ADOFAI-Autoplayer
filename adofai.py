e114ebff112a40e9e3cbca476ce39669fe8d947e
Steve28 <64412954+pl-Steve28-lq@users.noreply.github.com>
2021-02-26 20:47:28 +0900

    타이밍이 문제임 ㅋㅋ 아

--- adofai.py
+++ adofai.py
@@ -2,58 +2,37 @@ from pynput.keyboard import Key, Controller
 import json, time
 
 class ADOFAI:
-    def __init__(self, bpm = 100, pathdata = 'R'*9, offset = 0, eventdata = {}):
+    def __init__(self, bpm = 100, pathdata = [180]*9, offset = 0, speeds = {}):
         self.kb = Controller()
         self.bpm = bpm
-        key = list("RWHQGqUoTEJpRAMCBYDVFZNxL")
-        self.tileInfo = {key[i] : 15*i for i in range(len(key))}
-        self.pathdata = self.analyze(pathdata)
-        self.eventdata = eventdata
-        self.sec = 60/bpm
+        self.pathdata = pathdata
+        self.speedData = speeds
         self.offset = offset/1000
         self.length = len(self.pathdata)
-        self.twirled = 0
 
     def start(self):
-        print("[Start] BPM : " + str(self.bpm))
-        self.kb.press(Key.space)
-        self.kb.release(Key.space)
-        time.sleep(self.offset + 3 * self.sec + 1.25)
+        print(f"[Start] BPM : {self.bpm}")
+        self.press(Key.space)
+        slp = self.offset + 4 * self.sec() + 0.1
+        print(slp)
+        time.sleep(slp)
 
     def changeBPM(self, newBPM):
-        print("[Speed] BPM : " + str(self.bpm) + " => " + str(newBPM))
-        print(self.sec)
         self.bpm = newBPM
-        self.sec = 60/newBPM
-
+    
     def startMacro(self):
-        print(self.sec)
         self.start()
         tile = 0
         while tile < self.length:
-            delay = -0.1 + (360*self.twirled + (-2 * self.twirled + 1) * self.pathdata[tile])/180
+            delay = self.pathdata[tile]/180
             self.press()
-            print(self.pathdata[tile], delay)
-            eventcheck = self.eventdata.get(tile, None)
-            if isinstance(eventcheck, int):
-                self.changeBPM(eventcheck)
-            if isinstance(eventcheck, str):
-                self.twirled = 1 - self.twirled
+            event = self.speedData.get(tile+2, None)
+            if not event is None: self.changeBPM(event)
             tile += 1
-            time.sleep(delay * self.sec)
-
-    def analyze(self, pathdata):
-        processed = []
-        for i in range(len(pathdata)-1):
-            nowtile  = self.tileInfo[pathdata[i]]
-            nexttile = self.tileInfo[pathdata[i+1]]
-            #### TODO : 실험 안해봄 ####
-            angle = 180 - nowtile + nexttile
-            #### TODO : 실험 안해봄 ####
-            processed.append(angle)
-        #TODO : Processing Midspin Tile
-        return processed + [self.tileInfo[pathdata[-1]]]
+            time.sleep(delay * self.sec())
     
     def press(self, key='k'):
         self.kb.press(key)
         self.kb.release(key)
+
+    def sec(self): return 61/self.bpm
--- adofaimacro.py
+++ adofaimacro.py
@@ -1,4 +1,6 @@
 from adofai import ADOFAI
+import json
+
 
 def autoplay(levelpath):
     with open(levelpath, encoding='utf-8-sig') as f:
@@ -7,17 +9,56 @@ def autoplay(levelpath):
     bpm = settings['bpm']
     offset = settings['offset']
     pathdata = ctx['pathData']
+    realdata = []
     
     action = ctx['actions']
-    actiondata = {}
+    speedData = {}
+    twirlData = {}
 
-    for i in range(len(action)):
-        act = actions[i]
+    for act in action:
         event = act['eventType']
+        floor = act['floor']
         if event == 'SetSpeed':
-            actiondata[i] = act['beatsPerMinute']
+            speedData[floor] = act['beatsPerMinute']
         elif event == 'Twirl':
-            actiondata[i] = 'Twirl'
+            twirlData[floor] = 'Twirl'
+
+    key = list("RWHQGqUoTEJpRAMCBYDVFZNxL")
+    tileInfo = {key[i] : 15*i for i in range(len(key))}
+
+    print(speedData, twirlData)
+
+    idx = 0
+    before = 180
+    twirl = False
+    isMidspin = False
+    isNextOf = False
+    while idx < len(pathdata):
+        event = twirlData.get(idx, None)
+        if not event is None: twirl = not twirl
+
+        isMidspin = False
+        if idx != len(pathdata)-1:
+            isMidspin = pathdata[idx+1] == '!'
+        if isNextOf: idx += 1
+
+        angle = 0
+        prev = tileInfo[pathdata[idx]]
+        if isNextOf:
+            angle = (prev-before)%360
+        else:
+            angle = (180-before+prev)%360
+        realangle = angle if not twirl else 360-angle
+        realdata.append(realangle)
+        
+        before = prev
+        isNextOf = isMidspin
+        idx += 1
+
+    print(realdata)
     
-    macro = ADOFAI(bpm, pathdata, offset, actiondata)
+    macro = ADOFAI(bpm, realdata, offset, speedData)
     macro.startMacro()
+
+
+autoplay('./testlevel.adofai')
