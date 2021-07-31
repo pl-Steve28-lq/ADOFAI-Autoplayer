e3af91190d17a828dc9f7b4f6241f743f8a47148
Steve28 <64412954+pl-Steve28-lq@users.noreply.github.com>
2021-07-31 19:56:04 +0900

    코드 정리

--- ADOFAI/adofai.py
+++ ADOFAI/adofai.py
@@ -3,27 +3,21 @@ import json
 
 class ADOFAI:
   class File(Util.Singleton):
-    def __init__(self):
-      self.file = None
-      self.level = None
-      self.isPath = True
-
     def open(self, path):
-      self.file = open(path, encoding='utf-8-sig')
-      self.level = json.loads(self.file.read())
-      self.isPath = 'pathData' in self.level
-      self.event = {}
-      for i in filter(lambda x: x['eventType'] in ['SetSpeed', 'Twirl'], self.level['actions']):
+      file = open(path, encoding='utf-8-sig')
+      level = json.loads(file.read())
+      event = {}
+      for i in level['actions']:
         floor, etype = i['floor'], i['eventType']
-        if self.event.setdefault(floor):
-          self.event[floor] = self.event[floor][0], ADOFAI.Event.getEvent(i)
-        else:
-          self.event[floor] = (ADOFAI.Event.getEvent(i),)
-
-      return self._analyze(self.level, self.event)
+        if etype not in {'SetSpeed', 'Twirl'}: continue
+        event[floor] = \
+          (event[floor][0], ADOFAI.Event.getEvent(i)) \
+          if event.setdefault(floor) \
+          else (ADOFAI.Event.getEvent(i),)
+      return self._analyze(level, event)
 
     def _analyze(self, level, event):
-      if self.isPath: return self.parseTile(level, event)
+      if 'pathData' in level: return self.parseTile(level, event)
       else: Util.TODO()
 
     def parseTile(self, level, event):
@@ -32,15 +26,10 @@ class ADOFAI:
       L = len(path := level['pathData'])
       i = 0
       while i < L:
+        isMid = path[i] == '!'
+        i += isMid
         p = path[i]
-        isMid = False
-        if p == '!':
-          i += 1
-          isMid = True
-          p = path[i]
-        res.append(
-          ADOFAI.Tile.get(angle[p], event[i+1], isMid)
-        )
+        res.append(ADOFAI.Tile.get(angle[p], event[i+1], isMid))
         i += 1
       return res
         
@@ -60,10 +49,10 @@ class ADOFAI:
       evt = event['eventType']
       if evt == 'Twirl': return ADOFAI.Event.Twirl.get()
       if evt == 'SetSpeed':
-        if event['speedType'] == 'Bpm':
-          return ADOFAI.Event.BPMSpeed.get(event['beatsPerMinute'])
-        else:
-          return ADOFAI.Event.MultiplySpeed.get(event['bpmMultiplier'])
+        ev, val = ADOFAI.Event, 0
+        if event['speedType'] == 'Bpm': ev, val = ev.BPMSpeed, event['beatsPerMinute']
+        else: ev, val = ev.MultiplySpeed, event['bpmMultiplier']
+        return ev.get(val)
 
   File = File()
 
